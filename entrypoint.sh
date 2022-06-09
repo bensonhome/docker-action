171e9eedd041693c8466049ab043f4d892060207
Benson <410554565@qq.com>
2022-06-09 16:46:55 +0800

    :art:test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+echo "---> get input argument who-to-greet:"$INPUT_WHO-TO-GREET
+echo "---> get GITHUB_WORKSPACE:"$GITHUB_WORKSPACE
+echo "---> get GITHUB_REPOSITORY:"$GITHUB_REPOSITORY
+echo "---> get GITHUB_REF:"$GITHUB_REF
+echo "---> running task url:"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID
+
+echo "::warning file=entrypoint.sh,line=4,col=5,endColumn=7::Missing semicolon"
+echo "::error file=action.yml,line=3,col=1,endColumn=8::Line too long"
+echo "::error file=action.yml,line=5::Line too long"
+echo "::set-output name=SELECTED_COLOR::green"
+
 time=$(date)
 echo "::set-output name=time::$time"
